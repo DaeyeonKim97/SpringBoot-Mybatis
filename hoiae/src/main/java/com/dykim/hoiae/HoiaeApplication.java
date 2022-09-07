6d0e1a2599200534eeb8a8cf1e18bd0b6cd845dd
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-07 15:00:09 +0900

    Inititial commit

---
+++ hoiae/src/main/java/com/dykim/hoiae/HoiaeApplication.java
@@ -0,0 +1,13 @@
+package com.dykim.hoiae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoiaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoiaeApplication.class, args);
+	}
+
+}
---
+++ hoiae/src/test/java/com/dykim/hoiae/HoiaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dykim.hoiae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoiaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
