a602b6f20f91ce718e6bce66813e09403cf6325d
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-07 15:17:55 +0900

    Mybatis Configuration

---
+++ hoiae/src/main/java/com/dykim/hoiae/config/MybatisConfiguration.java
@@ -0,0 +1,10 @@
+package com.dykim.hoiae.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.dykim.hoiae")
+public class MybatisConfiguration {
+
+}
---
+++ hoiae/src/main/java/com/dykim/hoiae/model/dao/MemoMapper.java
@@ -0,0 +1,8 @@
+package com.dykim.hoiae.model.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemoMapper {
+
+}
