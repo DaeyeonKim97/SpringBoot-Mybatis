b768c9550dec192879447aec711c30635563c308
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-09-10 20:09:10 +0900

    Mybatis mapper

--- hoiae/src/main/java/com/dykim/hoiae/model/dao/MemoMapper.java
+++ hoiae/src/main/java/com/dykim/hoiae/model/dao/MemoMapper.java
@@ -1,8 +1,20 @@
 package com.dykim.hoiae.model.dao;
 
+import com.dykim.hoiae.model.dto.MemoDTO;
+import com.dykim.hoiae.model.dto.ReplyDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MemoMapper {
+    List<MemoDTO> selectAllMemo();
+    MemoDTO selectMemoByCode(int code);
+    int insertMemo(MemoDTO memo);
+    int updateMemo(MemoDTO memo);
+    int deleteMemo(int code);
 
+    List<ReplyDTO> selectReplyByMemoCode(int memoCode);
+    int insertReply(ReplyDTO reply);
+    int deleteReply(int code);
 }
